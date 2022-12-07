39a95b4c85a039ef692d89898d5cfcde5aef94a6
Handbrake brake <handbrake@mail.ru>
2022-12-08 00:06:30 +0300

    Quest 66 complete

--- Quest66/Program.cs
+++ Quest66/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.WriteLine("Введите первое значение:");
+int m;
+if (!int.TryParse(Console.ReadLine()!, out m))
+{
+    Console.WriteLine("Неверный ввод");
+    return;
+}
+
+Console.WriteLine("Введите второе значение:");
+int n;
+if (!int.TryParse(Console.ReadLine()!, out n))
+{
+    Console.WriteLine("Неверный ввод");
+    return;
+}
+
+if (n < m)
+{
+    Console.WriteLine("Неверный интервал");
+    return;
+}
+
+int sum = m;
+void SumInterval(int n, int m)
+{
+    if (n > m)
+    {
+        sum += n;
+        SumInterval(n - 1, m);
+    }
+    if (m == n)
+    {
+        Console.Write(n + " ");
+    }
+    else
+    {
+        Console.Write(n + " ");
+    }
+}
+SumInterval(n, m);
+Console.WriteLine($"Сумма = {sum}");
