b5d90a4a8b9c853ce2d2e4cd826a8f08752fc40f
Handbrake brake <handbrake@mail.ru>
2022-12-07 23:35:58 +0300

    Quest 64 complete

--- Quest64/Program.cs
+++ Quest64/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+Console.WriteLine("Задайте значение:");
+int n;
+if (!int.TryParse(Console.ReadLine()!, out n))
+{
+    Console.WriteLine("Неверный ввод");
+    return;
+}
+int m = 1;
+void ToOne(int n, int m)
+{
+
+    if (n > m)
+    {
+        ToOne(n, m + 1);
+    }
+    if (n > 1)
+    {
+        Console.Write(m + " ");
+    }
+    else
+    {
+        Console.Write(m);
+    }
+}
+
+ToOne(n, m);
